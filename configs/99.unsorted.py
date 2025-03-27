c38e12039aab28ac0f777c72bcf13ac4f0950646
Tin Lai <tin@tinyiu.com>
2025-03-27 13:13:04 +0000

    add configs examples
    Signed-off-by: Tin Lai <tin@tinyiu.com>

---
+++ configs/99.unsorted.py
@@ -0,0 +1,36 @@
+from registry import MANAGERS, Package
+
+
+shsh = MANAGERS["shsh"]
+(
+     shsh
+     << "soraxas/yadm"
+     << "soraxas/git-utils"
+     << "soraxas/open-rev-ports"
+     << "so-fancy/diff-so-fancy"
+     << "elasticdog/transcrypt"
+     << Package("tj/git-extras", "tj/git-extras -h pre='PREFIX=myroot make' -v ROOT=myroot")
+)
+
+
+fisher = MANAGERS["fisher"]
+(
+    fisher
+     << "soraxas/fisher"  # modified fisher that supports comments and omf
+     << "jorgebucaran/replay.fish"  # similar to bass, but with pure fish
+     << "patrickf1/fzf.fish"
+     << "wfxr/forgit"
+     << "jorgebucaran/autopair.fish"
+     # matchai/spacefish # for shell without starship
+     #
+     # soraxas/fish-cool-right-prompt
+     # IlanCosman/tide
+     << "soraxas/rgg.fish"  # rg with number selection
+     << "soraxas/fish-things"
+     << "jethrokuan/z"
+     << "otms61/fish-pet"
+     << "oakninja/makemefish"
+     << "otms61/fish-pet"
+     << "soraxas/breeze.fish"
+     << "acomagu/fish-async-prompt"
+)
